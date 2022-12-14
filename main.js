e80dbd61d151fa868887d35cb9401f818f79eeb7
RheatheCoder <87926743+RheatheCoder@users.noreply.github.com>
2022-12-15 10:25:57 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas (640, 480);
+    canvas.position (110, 250);
+    
+}
+
+function draw(){
+    circle ( 200, 200, 70);
+    fill ( 30, 0, 100);30
+    stroke (100, 0, 30);
+
+    rect (400, 110, 100, 100);
+    fill (100, 0, 20);
+
+    rect(30, 20, 55, 55, 20, 15, 10, 5);
+    fill (200, 0, 200);
+
+    circle ( 500, 5, 150);
+    fill ( 200, 0, 0);
+
+    ellipse (150, 300, 50, 30);
+    fill ( 0, 5, 5);
+
+    ellipse (230, 50, 200, 100 );
+    fill (50, 0, 50);
+
+
+
+
+
+
+
+
+}
